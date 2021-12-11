73275d0bdf516f8c4c6da3ce80a45b612037caaf
aditi0698 <95539573+aditi0698@users.noreply.github.com>
2021-12-11 16:23:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,14 +22,14 @@ function setup() {
 	video.hide();
 
 	poseNet = ml5.poseNet(video, modelLoaded);
-	poseNet.on('pose', gotPoses);
+	poseNet.on('pose', getPoses);
 }
 
 function modelLoaded() {
   console.log('PoseNet Is Initialized');
 }
 
-function gotPoses(results)
+function getPoses(results)
 {
   if(results.length > 0)
   {
@@ -103,4 +103,7 @@ function play()
 	song.play();
 	song.setVolume(1);
 	song.rate(1);
+}
+function Stop(){
+	song.stop();
 }
